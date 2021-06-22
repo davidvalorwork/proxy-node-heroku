84341dba2b3d7a1dea60b99a9b68c60b23888c36
David Valor <davidvalorwork@gmail.com>
2021-06-22 09:02:13 -0400

    update

---
+++ c.sh
@@ -0,0 +1,7 @@
+git config --global user.name 'David Valor'
+git config --global user.email 'davidvalorwork@gmail.com'
+git config credential.helper 'store --file ~/.credentialproxy'
+git add .
+git commit -am update
+git pull
+git push
